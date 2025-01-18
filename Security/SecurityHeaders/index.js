027c4ad1ab44f00216878321bc0ea0fb28c22800
Desh Deepak Singh <desh.s@iqm.com>
2025-01-18 20:30:50 +0530

    studied iframe protection and XSS example

--- Security/SecurityHeaders/index.js
+++ Security/SecurityHeaders/index.js
@@ -1,34 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
+// if not want the even first http request and redirect to https
 const redirectToHttps = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
+  if (req.headers["x-forwarded-proto"] !== "https") {
     // Redirect to HTTPS
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
   }
   next();
 };
 
 app.use(redirectToHttps);
 
-
 app.use((req, res, next) => {
-  res.setHeader('Referrer-Policy', 'no-referrer');
-  res.removeHeader('X-Powered-By');
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  res.setHeader("Referrer-Policy", "no-referrer"); // setting referrer info
+  res.removeHeader("X-Powered-By"); // to remove the info of application Or framework info
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
   next();
 });
 
-app.get('/list', (req, res) => {
-  res.send([{
-    id: 1,
-    title: "Namaste Frontend System Design"
-  }])
+app.use(express.static("public"));
+
+app.get("/list", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: "Namaste Frontend System Design",
+    },
+  ]);
+  // res.sendFile(__dirname + "/public/hello.html");
 });
 
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5012;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- Security/XSS/mitigation/index.js
+++ Security/XSS/mitigation/index.js
@@ -3,22 +3,27 @@ const express = require("express");
 const PORT = 3010;
 const app = express();
 
+// middleware
 app.use((req, res, next) => {
-    res.setHeader(
-        'Content-Security-Policy',
-        "default-src 'self';" + 
-        "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;"
-    );
-    next();
-})
+  // default-src 'self' will only let you load soruces that is from your own route (current localhost:3010)
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self';" +
+      "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;" // self and list ('nonce=random key , unsage inline , unsecure.com)
+    // unsage-inline : execute inline scripts
+    // nonce : to define the trusted script which need to be loaded
+    // if nonce is not provided all the script will be loaded
+  );
+  next();
+});
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log(req.url);
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started at http://locolhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
